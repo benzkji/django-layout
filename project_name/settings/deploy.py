91dd7ffc6a4f9c9da9274fe44f59b099fb4cd274
benzkji <bnzk@bnzk.ch>
2015-05-19 14:52:32 +0200

    the ADD

---
+++ project_name/settings/deploy.py
@@ -0,0 +1,9 @@
+
+# common for all deployment settings
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+THUMBNAIL_DEBUG = False
+FOLDERLESS_DEBUG = False
+
+COMPRESS_ENABLED = True
---
+++ project_name/settings/hosts.py
@@ -0,0 +1,7 @@
+
+# good to forget, so it gets a file on its own.
+
+ALLOWED_HOSTS = (
+    'agv-ag.ch',
+    '.cubegrafik1.nine.ch',
+)
