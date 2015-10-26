bc772108cfcb7ddf471e69050ca6173020ad3ce0
benzkji <bnzk@bnzk.ch>
2015-10-26 13:02:43 +0100

    add iconfont to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    iconfont = require('gulp-iconfont'),
+    consolidate = require('gulp-consolidate'),
     autoprefixer = require('gulp-autoprefixer'),
     shell = require('gulp-shell'),
     jshint= require('gulp-jshint'),
@@ -10,17 +12,41 @@ var gulp = require('gulp'),
 require('es6-promise').polyfill();
 var static_path = './{{ project_name }}/static/{{ project_name }}/';
 
-
 gulp.task('sass', function () {
     gulp.src(static_path + 'sass/screen.sass')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer("last 2 versions"))
         .pipe(gulp.dest(static_path + 'css/'))
-        .pipe(livereload());
+});
+
+gulp.task('icons', function () {
+    var runTimestamp = Math.round(Date.now()/1000);
+    return gulp.src([static_path + 'iconfont/svg/*.svg'])
+        .pipe(iconfont({
+                fontName: 'icons', // required
+                appendUnicode: true, // recommended option
+                formats: ['ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available
+                timestamp: runTimestamp // recommended to get consistent builds when watching files
+            }
+        )
+    ).on('glyphs', function(glyphs, options) {
+        // CSS templating, e.g.
+        console.log(glyphs, options);
+        gulp.src(static_path + 'iconfont/sass/_iconfont.scss')
+            .pipe(consolidate('lodash', {
+                glyphs: glyphs,
+                fontName: 'icons',
+                fontPath: '../iconfont/font/',
+                className: 'icon'
+            })
+        )
+        .pipe(gulp.dest(static_path + 'sass/'));
+    })
+    .pipe(gulp.dest(static_path + 'iconfont/font/'));
 });
 
 gulp.task('jshint', function () {
-    gulp.src(['gulpfile.js', wti_static + 'js/**.js'])
+    gulp.src(['gulpfile.js', static_path + 'js/**.js'])
         .pipe(jshint())
         .pipe(livereload());
 });
@@ -39,13 +65,15 @@ gulp.task('pip-compile', shell.task(
     )
 );
 
-gulp.task('default', ['sass', 'jshint', 'flake8']);
+gulp.task('default', ['sass', 'pip-compile', 'jshint', 'flake8']);
 
 gulp.task('watch', function () {
     livereload.listen();
-    gulp.watch(['./**/*.html', './**/*.py']).on('change', livereload.changed);
-    gulp.watch(static_path + '**/*.sass', ['sass']);
+    gulp.watch(['./**/*.html', './**/*.py', './**/*.css']).on('change', livereload.changed);
+    gulp.watch(static_path + 'sass/*.sass', ['sass']);
+    gulp.watch(static_path + 'iconfont/svg/*.svg', ['iconfont']);
     gulp.watch(['gulpfile.js', static_path + 'js/**.js'], ['jshint']);
     gulp.watch('./**/*.py', ['flake8']);
     gulp.watch('./requirements/*.in', ['pip-compile']);
 });
+
