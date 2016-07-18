c0089cd0b72b048586e444e5eec6f66cc39fbf15
benzkji <bnzk@bnzk.ch>
2016-07-18 11:47:26 +0200

    sane sass, dummy cms plugin added

---
+++ apps/djangocms_my/cms_plugins.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from django.utils.translation import ugettext_lazy as _
+
+from .models import My
+
+
+class MyPlugin(CMSPluginBase):
+    model = My
+    name = _(u'MyPlugin')
+    render_template = "djangocms_my/my.html"
+    text_enabled = False
+
+    def render(self, context, instance, placeholder):
+        context['plugin'] = self
+        context['instance'] = instance
+        context['placeholder'] = placeholder
+        return context
+
+plugin_pool.register_plugin(MyPlugin)
---
+++ apps/djangocms_my/models.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from django.utils.encoding import python_2_unicode_compatible
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from cms.models.pluginmodel import CMSPlugin
+
+
+@python_2_unicode_compatible
+class My(CMSPlugin):
+    amount = models.CharField(max_length=255)
+
+    def __str__(self):
+        return u'%s Columns' % (self.amount, )
