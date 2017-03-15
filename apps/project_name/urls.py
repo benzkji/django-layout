5feda1affc98c817c173f38f44997e8caf088f77
benzkji <bnzk@bnzk.ch>
2017-03-15 08:53:11 +0100

    urls.py cleanup

--- apps/project_name/urls.py
+++ apps/project_name/urls.py
@@ -1,18 +1,6 @@
-from django.conf.urls.static import static
-from django.conf.urls import patterns, include
-from django.conf import settings
-
-from django.contrib import admin
+from django.conf.urls import url
 
 
-# maybe use i18npatterns?!
 urlpatterns = [
-    # (r'', include('{{ project_name }}.apps.')),
     url(r'^(?P<slug>[a-z0-9]+)/$', WhatEver.as_view(), name='whatever'),
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/', include(admin.site.urls)),
 ]
-
-
-if settings.DEBUG and settings.MEDIA_ROOT:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
