ef101614b9f38b228a9f9ae9e165969a8ba2976a
benzkji <bnzk@bnzk.ch>
2017-12-22 11:42:02 +0100

    more boilerplate

--- apps/project_name/cms_toolbars.py
+++ apps/project_name/cms_toolbars.py
@@ -1,33 +1,21 @@
 # coding: utf-8
-
 from __future__ import unicode_literals
 
-from django.core.urlresolvers import reverse
-from django.utils.translation import ugettext_lazy as _
+# from django.utils.translation import ugettext_lazy as _
 from cms.toolbar_pool import toolbar_pool
-from cms.toolbar.items import Break
-from cms.cms_toolbars import ADMIN_MENU_IDENTIFIER, ADMINISTRATION_BREAK
-from cms.toolbar_base import CMSToolbar
+# from cms.utils.urlutils import admin_reverse
+from djangocms_misc.alternate_toolbar.cms_toolbars import AlternateBasicToolbar
+
+
+toolbar_pool.unregister(AlternateBasicToolbar)
 
 
 @toolbar_pool.register
-class CustomToolbar(CMSToolbar):
+class CustomToolbar(AlternateBasicToolbar):
 
-    def populate(self):
-        admin_menu = self.toolbar.get_or_create_menu(ADMIN_MENU_IDENTIFIER)
-
-        break_position = admin_menu.find_first(Break, identifier=ADMINISTRATION_BREAK)
-        url = reverse('admin:password_change')
-        admin_menu.add_sideframe_item(_("Passwort ändern"), url=url, position=break_position + 1)
-
-        url = reverse('admin:filer_folder_changelist')
-        admin_menu.add_sideframe_item(_('Files'), url=url, position=1)
-
-        # blog_menu = self.toolbar.get_or_create_menu("bellevue_blog", "Bellevue Blog",
-        #                                                 position=3, )
-        # url = reverse('admin:phblog_blogpost_changelist')
-        # blog_menu.add_sideframe_item(_('Posts'), url=url)
-        # url = reverse('admin:phblog_category_changelist')
-        # blog_menu.add_sideframe_item(_('Kategorien'), url=url)
-        # url = reverse('admin:phblog_tag_changelist')
-        # blog_menu.add_sideframe_item('Tags', url=url)
+    def add_more_admin_menu_items(self):
+        pass
+        # self.admin_menu.add_sideframe_item(
+        #     _('Projects'),
+        #     url=admin_reverse('newsevents_event_changelist')
+        # )
