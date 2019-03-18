ac648f4770beb3a7b2a7962621a690e947f7446c
benzkji <bnzk@bnzk.ch>
2019-03-18 10:36:46 +0100

    settings simple revisited

--- project/settings-simple/apps.py
+++ project/settings-simple/apps.py
@@ -9,11 +9,23 @@ INSTALLED_APPS = [
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
-    '{{ project_name }}',
+    'lokal_int',
+
+    # 'djangocms_misc',
+    'formfieldstash',
+    'ckeditor_link',
+    'ckeditor_link.link_model',
+    'mailprotector',
+    'textblocks',
+    # 'filer_addons.filer_utils',
+    # 'filer_addons.filer_gui',
+    # 'filer_addons.filer_signals',
 
     'compressor',
-    # 'easy_thumbnails',
+    'ckeditor',
 
-    # Uncomment the next line to enable the admin:
+    'filer',
+    'easy_thumbnails',
+
     'django.contrib.admin',
 ]
