0cf4114715c79b721b36df0d84013d858933bdac
benzkji <bnzk@bnzk.ch>
2019-04-29 15:09:21 +0200

    some enhancement for baseplugin custom plugin development

--- apps/djangocms_my/cms_plugins.py
+++ apps/djangocms_my/cms_plugins.py
@@ -2,21 +2,20 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext_lazy as _
+from djangocms_baseplugins.baseplugin.cms_plugins import BasePluginMixin
+from djangocms_baseplugins.baseplugin.utils import build_baseplugin_fieldset
+from djangocms_baseplugins.baseplugin import defaults
 
 from .models import My
 
 
-class MyPlugin(CMSPluginBase):
+@plugin_pool.register_plugin
+class MyPlugin(BasePluginMixin, CMSPluginBase):
     model = My
     name = _(u'My')
-    render_template = "djangocms_my/my.html"
-    text_enabled = False
-
-    def render(self, context, instance, placeholder):
-        context['plugin'] = self
-        context['instance'] = instance
-        context['placeholder'] = placeholder
-        return context
-
-
-plugin_pool.register_plugin(MyPlugin)
+    render_template = "djangocms_baseplugins/my.html"
+    fieldsets = build_baseplugin_fieldset(**{
+        'design': ['background', ],  # create a form if you want dropdowns!
+        'content': ['amount', ],
+        'advanced': defaults.BASEPLUGIN_ADVANCED_FIELDS,
+    })
--- apps/djangocms_my/models.py
+++ apps/djangocms_my/models.py
@@ -3,12 +3,21 @@ from __future__ import unicode_literals
 
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
-from cms.models.pluginmodel import CMSPlugin
+from djangocms_baseplugins.baseplugin.models import AbstractBasePlugin
 
 
 @python_2_unicode_compatible
-class My(CMSPlugin):
+class My(AbstractBasePlugin):
     amount = models.CharField(max_length=255)
 
     def __str__(self):
-        return u'%s Columns' % (self.amount, )
+        text = self.amount
+        return self.add_hidden_flag(text)
+
+    # def copy_relations(self, old_instance):
+    #     super().copy_relations(old_instance)
+    #     # self.images.add(*old_instance.images.all())
+    #     for entry in old_instance.teaserimage_set.all():
+    #         entry.id = None
+    #         entry.save()
+    #         self.teaserimage_set.add(entry)
