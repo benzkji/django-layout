44319031517c3c2bb8163b1f8561ed13f30ebf0e
Ben Stähli <bnzk@bnzk.ch>
2020-09-22 15:08:50 +0200

    introduce seo base

--- apps/project_name/models.py
+++ apps/project_name/models.py
@@ -9,6 +9,10 @@ from filer.fields.file import FilerFileField
 from ckeditor_link.link_model.models import CMSFilerLinkBase
 
 
+class Link(CMSFilerLinkBase):
+    pass
+
+
 class PublishedBase(models.Model):
     published = models.BooleanField(default=False)
 
@@ -24,5 +28,33 @@ class ModifiedBase(models.Model):
         abstract = True
 
 
-class Link(CMSFilerLinkBase):
-    pass
+class SEOBase(models.Model):
+    seo_title = models.CharField(
+        max_length=255,
+        default='',
+        blank=True,
+    )
+    meta_description = models.CharField(
+        max_length=255,
+        default='',
+        blank=True,
+    )
+
+    class Meta:
+        abstract = True
+
+    def get_seo_title(self):
+        if getattr(self, 'seo_title', None):
+            return self.seo_title
+        if getattr(self, 'name', None):
+            return self.name
+        if getattr(self, 'title', None):
+            return self.title
+        return ''
+
+    def get_seo_description(self):
+        if getattr(self, 'meta_description', None):
+            return self.meta_description
+        if getattr(self, 'description', None):
+            return self.description
+        return ''
