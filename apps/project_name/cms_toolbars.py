7a950ae6d0179fff7cdb3b9eb47fba29c620407a
Ben Stähli <bnzk@bnzk.ch>
2021-03-23 16:13:40 +0100

    toolbar setup

--- apps/project_name/cms_toolbars.py
+++ apps/project_name/cms_toolbars.py
@@ -1,9 +1,6 @@
-# coding: utf-8
-from __future__ import unicode_literals
-
-# from django.utils.translation import ugettext_lazy as _
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 from cms.toolbar_pool import toolbar_pool
-# from cms.utils.urlutils import admin_reverse
 from djangocms_misc.alternate_toolbar.cms_toolbars import AlternateBasicToolbar
 
 
@@ -26,8 +23,9 @@ class CustomToolbar(AlternateBasicToolbar):
 
     def add_more_admin_menu_items(self):
 
-        self.add_crud_menu_item('admin', 'log', label='Admin Log')
+        # misc_menu = self.admin_menu.get_or_create_menu("misc", _("Diverses"),  )
 
+        self.add_crud_menu_item('admin', 'log', label=_('Admin Log'))
         # self.admin_menu.add_sideframe_item(
         #     _('Projects'),
         #     url=admin_reverse('newsevents_event_changelist')
