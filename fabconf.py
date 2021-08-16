cd4d40304f12294b960882fad8a4d810e1d9c886
Ben Stähli <bnzk@bnzk.ch>
2021-08-16 11:12:51 +0200

    #46, simple one ;-)

--- fabconf.py
+++ fabconf.py
@@ -12,7 +12,7 @@ env.is_supervisord = True
 env.is_nginx_gunicorn = True
 env.is_uwsgi = False
 env.is_apache = False
-env.remote_ref = 'origin/master'
+env.remote_ref = 'origin/main'
 # these will be checked for changes
 env.requirements_files = [
     'requirements/deploy.txt',
