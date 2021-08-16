a2adc690f81645cb6ab8fd9888322f98e720e31e
Ben Stähli <bnzk@bnzk.ch>
2021-08-16 11:10:58 +0200

    #45 #close

--- project/settings/base.py
+++ project/settings/base.py
@@ -1,88 +1,62 @@
-# coding: utf-8
-
 import os
 import sys
+import subprocess
 
 # just in case - know the defaults ;-)
 # import django.conf.global_settings as DEFAULT_SETTINGS
 
-PROJECT_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                            '../../')
+# helpers and such
+PROJECT_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../')
 sys.path.append(os.path.join(PROJECT_PATH, 'apps/'))
-
-# the fake
 ugettext = lambda s: s  # noqa
 
+
+# some django things
 ADMINS = [
     # no more! raven FTW. ('BNZK', 'support@bnzk.ch'),
 ]
 MANAGERS = ADMINS
 DEFAULT_FROM_EMAIL = '{{project_name}}@bnzk.ch'
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
+# enable when behind nginx proxy
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+INTERNAL_IPS = (
+    '127.0.0.1',
+    '0.0.0.0',
+)
+MIGRATION_MODULES = {
+    'textblocks': 'apps.{{ project_name }}.migrations_textblocks',
+    # 'image': 'apps.{{ project_name }}.migrations_plugins.image',
+}
+SITE_ID = 1
+ROOT_URLCONF = 'project.urls'
+# default time zone
+TIME_ZONE = 'Europe/Zurich'
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
+USE_I18N = True
+USE_L10N = True
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+WSGI_APPLICATION = 'project.wsgi.application'
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+SECRET_KEY = '{{ secret_key }}'
+
+
+# some third party things
 
 # opposite of DEBUG!
 # HTML_MINIFY = True
 # add p when needed
 # EXCLUDE_TAGS_FROM_MINIFYING = ['custom', 'pre', 'textarea', 'p', 'script', 'style', ]
 KEEP_COMMENTS_ON_MINIFYING = True
-
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
 TEXTBLOCKS_SHOWKEY = True
-
-# enable when behind nginx proxy
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-
 # SENTRY_DSN = 'https://xxxxxx@sentry.io/1416363'
 SENTRY_DSN = False
+VERSION = subprocess.check_output(['git', 'describe', '--tags']).strip()
 
-def get_git_describe():
-    import subprocess
-    return subprocess.check_output(['git', 'describe', '--tags']).strip()
 
-VERSION = get_git_describe()
-
-INTERNAL_IPS = (
-    '127.0.0.1',
-    '0.0.0.0',
-)
-
-MIGRATION_MODULES = {
-    'textblocks': 'apps.{{ project_name }}.migrations_textblocks',
-    # 'image': 'apps.{{ project_name }}.migrations_plugins.image',
-}
-
-ROOT_URLCONF = 'project.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'project.wsgi.application'
-
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
-# only use when having redis or file cache backend!
-# SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Europe/Zurich'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '{{ secret_key }}'
-
+# middleware and templates
 MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
